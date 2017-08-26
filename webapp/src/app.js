93ad95ee7b56fef5cfc527ff3664b165196e56f4
qiang <957932921@qq.com>
2017-08-26 14:22:15 +0800

    首页界面

--- webapp/src/app.js
+++ webapp/src/app.js
@@ -2,10 +2,18 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router/index'
 import store from './vuex/store'
-
+import  ElementUI from 'element-ui'
+import 'element-ui/lib/theme-default/index.css'
+import Mint from 'mint-ui';
+Vue.use(Mint);
+Vue.use(ElementUI);
 new Vue({
   el: '#app',
   router,
   store,
   render: h => h(App)
 })
+var designWidth = 375, rem2px = 100;
+document.documentElement.style.fontSize =
+((window.innerWidth / designWidth) * rem2px) + 'px';
+console.log(document.documentElement.style.fontSize)
